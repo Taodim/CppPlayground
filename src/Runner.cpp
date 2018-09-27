6ee4ee52aa5d815ec664e48a6cbc8911b0856fa2
tdao <tdao@ROC-01.sd.spawar.navy.mil>
2018-09-27 15:35:56 -0700

    -played with vectors

--- src/Runner.cpp
+++ src/Runner.cpp
@@ -7,11 +7,29 @@
 
 #include "Runner.h"
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
 int main()
 {
-    cout << "Hello, World!" << endl;
-    cout << "testing things out right now" << endl; //testing things out
+	//TESTING Vectors
+    cout << "Creating Vector..." << endl;
+
+    vector<int> intVect;
+
+    intVect.push_back(1);
+    intVect.push_back(2);
+    intVect.push_back(3);
+    intVect.push_back(4);
+    intVect.push_back(5);
+
+    for (int i = 0; i < intVect.size(); i++){
+    	cout << intVect[i] << ", ";
+    }
+
+    cout << binary_search(intVect.begin(), intVect.end(), 9) << endl;
+
     return 0;
 }
