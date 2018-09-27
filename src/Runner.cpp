6f420a0465fbccc96028d309ab4b08f47637ca65
Tim Dao <timdao4@yahoo.com>
2018-09-26 20:02:45 -0700

    -INITIAL

---
+++ src/Runner.cpp
@@ -0,0 +1,17 @@
+/*
+ * Runner.cpp
+ *
+ *  Created on: Sep 26, 2018
+ *      Author: timdao4
+ */
+
+#include "Runner.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+    cout << "testing things out right now" << endl; //testing things out
+    return 0;
+}
---
+++ src/Runner.h
@@ -0,0 +1,14 @@
+/*
+ * Runner.h
+ *
+ *  Created on: Sep 26, 2018
+ *      Author: timdao4
+ */
+
+#ifndef RUNNER_H_
+#define RUNNER_H_
+
+class Runner {
+};
+
+#endif /* RUNNER_H_ */
